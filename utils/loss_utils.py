a0d849bba3bc1595d81935d1074b2ce1fad12983
JuanCervino <JuanCervino>
2021-12-09 20:29:31 -0500

    First Version

--- utils/loss_utils.py
+++ utils/loss_utils.py
@@ -9,11 +9,13 @@ def focal_loss(f_data, label, gamma=2, num_classes=10):
     p_c = torch.sum(p*label, dim=1)
     return torch.mean( - (1-p_c) ** gamma * p_c.log())
 
-def alpha_loss(f_data,label,alpha=0.8):
+def alpha_loss(f_data,label, num_classes=10 ,alpha=0.8):
     if alpha == 1.0:
         loss = torch.mean(torch.sum(-target*torch.log(torch.softmax(f_data,dim=1) + 1e-8),dim=1))
     else:
-        alpha_cuda = torch.FloatTensor([alpha]).cuda()
-        one_cuda = torch.FloatTensor([1.0]).cuda()
-        loss = (alpha_cuda/(alpha_cuda-one_cuda))*torch.mean(torch.sum(label*(one_cuda - torch.softmax(f_data,dim=1).pow(one_cuda - (one_cuda/alpha_cuda))),dim=1))
-    return loss
+        # alpha_cuda = torch.FloatTensor([alpha]).cuda()
+        # one_cuda = torch.FloatTensor([1.0]).cuda()
+        p = _softmax(f_data)
+        label = F.one_hot(label, num_classes=num_classes)
+        p_c = torch.sum(p * label, dim=1)
+    return torch.mean( (alpha/(alpha-1))* (1-p_c) ** (1-1/alpha) )
