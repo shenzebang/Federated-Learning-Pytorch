d89b7fed9455fa8f869e4f17b2f823448c3659bf
ihounie <ihounie@gmail.com>
2023-02-21 09:21:46 -0500

    resilient and pd params

--- core/imbalance_fl.py
+++ core/imbalance_fl.py
@@ -3,6 +3,7 @@ from tqdm import trange
 from collections import namedtuple
 import torch
 
+
 ImFL_server_state = namedtuple("ImFL_server_state", ['global_round', 'model', 'lambda_var'])
 
 
@@ -28,3 +29,28 @@ class ImbalanceFL(PrimalDualFedAlgorithm):
             print(lambda_new)
         # print('client_losses', client_losses)
         # print('weights', weights)
+
+ImFL_server_state_res = namedtuple("ImFL_server_state_res", ['global_round', 'model', 'lambda_var', 'perturbation'])
+
+
+class ImbalanceFLRes(PrimalDualFedAlgorithm):
+    def __init__(self, fed_algorithm: FedAlgorithm, config, logger, auxiliary_data=None):
+        super(ImbalanceFLRes, self).__init__(fed_algorithm, config, logger, auxiliary_data)
+
+    def server_init(self) -> ImFL_server_state_res:
+        model = self.primal_fed_algorithm.server_state.model
+        lambda_var = torch.zeros(self.config.n_workers)
+        perturbation = torch.zeros(self.config.n_workers)
+        return ImFL_server_state_res(global_round=0, model=model, lambda_var=lambda_var, perturbation=perturbation)
+
+    def step(self):
+        sss = self.server_state
+        weights = (1. + sss.lambda_var - torch.mean(sss.lambda_var))
+        client_losses = torch.tensor(self.primal_fed_algorithm.clients_evaluate())
+        self.primal_fed_algorithm.fit(weights, self.config.n_p_steps)
+        model_new = self.primal_fed_algorithm.server_state.model
+        lambda_new = sss.lambda_var + self.config.lambda_lr * (client_losses - torch.mean(client_losses) - self.config.tolerance_epsilon) / self.config.n_workers
+        lambda_new = torch.clamp(lambda_new, min=0., max=100.)
+        perturb_new =  sss.perturbation + self.config.perturbation_lr * (-self.config.perturbation_penalty * sss.perturbation + sss.lambda_var)
+        perturb_new = torch.clamp(lambda_new, min=0.)
+        self.server_state = ImFL_server_state_res(global_round=sss.global_round+1, model=model_new, lambda_var=lambda_new, perturbation=perturb_new)
--- run_pd.sh
+++ run_pd.sh
@@ -1,9 +1,8 @@
 #P_FL
 
-python run_PD_FL.py --dataset cifar10 --n_workers_per_round 100\
-          --reduce_to_ratio .2 --use_ray --imbalance --n_minority 1\
-            --formulation imbalance-fl --learner fed-avg --local_lr 5e-2\
-              --n_pd_rounds 1000 --loss_fn cross-entropy-loss \
-                --n_workers 100 --n_p_steps 5  --no_data_augmentation\
-                  --lambda_lr 2 --tolerance_epsilon .01 --use_gradient_clip --n_minority 1\
-                  --heterogeneity dir --dir_level 0.6
+python run_PD_FL.py --dataset mnist --homo_ratio 0.0 --n_workers_per_round 100\
+	  --reduce_to_ratio .05 --use_ray --imbalance\
+	    --formulation imbalance-fl --learner fed-avg --local_lr 5e-2\
+	      --n_pd_rounds 500 --loss_fn cross-entropy-loss --model mlp\
+	        --n_workers 100 --n_p_steps 5 --dense_hid_dims 128-128 --no_data_augmentation\
+		  --lambda_lr 2 --tolerance_epsilon .01 --use_gradient_clip
